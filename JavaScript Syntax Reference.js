dd7edd270b5d5889cb2fc9040cc27ef871ee1993
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2022-12-28 13:33:15 +0000

    Update JavaScript Syntax Reference.js

--- JavaScript Syntax Reference.js
+++ JavaScript Syntax Reference.js
@@ -1,2 +1,8 @@
 Lambda 
 const goals = (a,b,c) => a + b + c;
+
+
+String Manipulation
+//map: creates a new array from calling a function for every array element.
+//split the string, map each character and duplicate it, then join and return the result.
+var doubleChar = (str) => str.split("").map(c => c + c).join("");
