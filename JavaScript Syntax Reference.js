dc5e13b929899bdf3f924ee13e3a3da2294668c9
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2022-12-29 12:07:22 +0000

    Update JavaScript Syntax Reference.js

--- JavaScript Syntax Reference.js
+++ JavaScript Syntax Reference.js
@@ -6,3 +6,9 @@ String Manipulation
 //map: creates a new array from calling a function for every array element.
 //split the string, map each character and duplicate it, then join and return the result.
 var doubleChar = (str) => str.split("").map(c => c + c).join("");
+
+//return sentence with name.
+const greet = (name) => 'Hello, ' + name + ' how are you doing today?'
+
+//string interpolation
+const greet = name => `Hello, ${name} how are you doing today?`;
