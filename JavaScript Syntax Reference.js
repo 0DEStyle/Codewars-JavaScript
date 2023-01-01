0cb4fba918aafd42576e9c0060dbe79562b16e87
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2023-01-01 09:44:05 +0000

    Update JavaScript Syntax Reference.js

--- JavaScript Syntax Reference.js
+++ JavaScript Syntax Reference.js
@@ -1,8 +1,18 @@
+print
+console.log(x)
+
+
 Lambda 
 const goals = (a,b,c) => a + b + c;
 
 
 String Manipulation
+//reverse string
+const str =  str.split("").reverse().join("");
+
+//to lower case
+const str = str.toLowerCase() == str.toLowerCase();
+
 //map: creates a new array from calling a function for every array element.
 //split the string, map each character and duplicate it, then join and return the result.
 var doubleChar = (str) => str.split("").map(c => c + c).join("");
