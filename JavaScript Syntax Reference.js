b4618a1d5d4af9b2946fb7f540cb7aa17c09539b
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2023-01-04 12:35:50 +0000

    Update JavaScript Syntax Reference.js

--- JavaScript Syntax Reference.js
+++ JavaScript Syntax Reference.js
@@ -27,3 +27,30 @@ note!!!!! don't use ' apostrophe , use ` grave accent, or it won't work
 - Array -
 //check array include element
 const include = (arr, item) => arr.includes(item);
+
+//Generate a sequence of number
+const arr = n => Array.from({length: n}, (_, i) => i); // 0 , .... , n-1 // underscore _ is just a valid variable number
+//or, store digits using arr.push(i)
+var newArr = [];
+  for(var i = 0; i < n; i++){
+    newArr.push(i);
+  }
+
+//print array index
+const array1 = ['a', 'b', 'c'];
+const iterator = array1.keys();
+
+for (const key of iterator) 
+  console.log(key);
+//> 0,1,2
+
+//print array index using Spread syntax (...)
+const arr = (n = 0) => [...Array(n).keys()]
+console.log(arr(5));
+//> Array [0, 1, 2, 3, 4]
+
+//print array
+const array1 = ['a', 'b', 'c'];
+for (const item of array1) {
+  console.log(item);
+}
