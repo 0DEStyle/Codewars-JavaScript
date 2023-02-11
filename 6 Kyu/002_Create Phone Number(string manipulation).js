0791a6a863c8059e1ddd0476c0d766b924f0c97a
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2023-02-11 13:48:33 +0000

    Update 002_Create Phone Number(string manipulation).js

--- 6 Kyu/002_Create Phone Number(string manipulation).js
+++ 6 Kyu/002_Create Phone Number(string manipulation).js
@@ -13,6 +13,11 @@ Don't forget the space after the closing parentheses!
 //Then simiplfied into one line by using an Lambda expression for methods.
 createPhoneNumber = n =>`(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`;
 
+//alternative
+function createPhoneNumber(numbers){
+  return numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
+}
+
 //Solution 2 replace method
 function createPhoneNumber(numbers){
   var format = "(xxx) xxx-xxxx";
